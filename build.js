28a1c1edae8517e094de458f24b577bd5b104cd6
Tricked <tricked@tricked.pro>
2022-12-11 17:35:04 +0100

    fix maybe

--- build.js
+++ build.js
@@ -6,21 +6,19 @@ import PKG from "./package.json" assert { type: "json" };
 console.log("Building...");
 
 const { result } = concurrently(["pnpm:build:*"]);
-
-result.then(async () => {
-  console.log("Done building");
+await result;
+console.log("Done building");
+try {
+  await mkdir("./dist");
+} catch (_) {}
+for await (let file of await readdir("./build")) {
+  if (!file.endsWith(".zip")) continue;
   try {
-    await mkdir("./dist");
-  } catch (_) {}
-  for await (let file of await readdir("./build")) {
-    if (!file.endsWith(".zip")) continue;
-    try {
-      await rename(
-        "./build/" + file,
-        `./dist/${PKG.name}-${PKG.version}-${file.split("-")[0]}.zip`,
-      );
-    } catch (error) {
-      console.error(error);
-    }
+    await rename(
+      "./build/" + file,
+      `./dist/${PKG.name}-${PKG.version}-${file.split("-")[0]}.zip`,
+    );
+  } catch (error) {
+    console.error(error);
   }
-});
+}
