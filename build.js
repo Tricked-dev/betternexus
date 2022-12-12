79e6962f16c73fee6dc418d3ecc71138a0af3a07
Tricked <tricked@tricked.pro>
2022-12-12 21:21:28 +0100

    revise the build script & increase version

--- build.js
+++ build.js
@@ -1,28 +1,23 @@
 import { concurrently } from "concurrently";
-import { mkdir, readdir, rename } from "fs/promises";
+import { mkdir, readdir, readFile, rename } from "fs/promises";
 
-import PKG from "./package.json" assert { type: "json" };
+console.log("Reading package.json...");
+const { name, version } = JSON.parse(await readFile("./package.json", "utf-8"));
 
 console.log("Building...");
-
 // run all build script for browsers
 const { result } = concurrently(["pnpm:build:*"]);
 await result;
 
 console.log("Done building");
 // create dist folder if it doesn't exist already
-try {
-  await mkdir("./dist");
-} catch (_) {}
+await mkdir("./dist").catch(() => {});
 // rename all zip files to include version number and browser name
 for await (let file of await readdir("./build")) {
   if (!file.endsWith(".zip")) continue;
-  try {
-    await rename(
-      "./build/" + file,
-      `./dist/${PKG.name}-${PKG.version}-${file.split("-")[0]}.zip`,
-    );
-  } catch (error) {
-    console.error(error);
-  }
+  let browser = file.split("-")[0];
+  await rename(
+    `./build/${file}`,
+    `./dist/${name}-${version}-${browser}.zip`,
+  ).catch(console.error);
 }
