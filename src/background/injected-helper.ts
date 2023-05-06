7d76272fb9230f2bf613425351f6a26886d4b52c
Tricked <tricked@tricked.pro>
2023-05-06 17:09:18 +0200

    login error

--- src/background/injected-helper.ts
+++ src/background/injected-helper.ts
@@ -1,185 +1,206 @@
 const windowChanger = (config: Config) => {
+  //@ts-ignore
+  let loggedIn = typeof window.USER_ID == "number"
+  if (!loggedIn && !document.getElementById("better-nexus-thing")) {
+    let p = document.createElement("p")
+    p.id = "better-nexus-thing"
+    p.innerText =
+      "Better Nexus requires you to login otherwise it will not work"
+    p.style.background =
+      "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 58%, rgba(0,212,255,1) 100%)"
+    p.style.textAlign = "center"
+    p.style.color = "white"
+    p.style.padding = "10px"
+    p.style.fontFamily = "roboto"
+    p.style.fontSize = "2rem"
+    document.body.append(p.cloneNode(true))
+    document.body.prepend(p)
+  }
   // The download path @example https://www.nexusmods.com/subnautica/mods/12?tab=files&file_id=4226
-  let path: string;
+  let path: string
   const addQuickDownload = () => {
-    if (document.getElementById("better-nexus-active")) return;
-    const manualParentBase = document.getElementById("action-manual");
-    const manualParent = manualParentBase?.cloneNode(true) as HTMLElement;
+    if (document.getElementById("better-nexus-active")) return
+    const manualParentBase = document.getElementById("action-manual")
+    const manualParent = manualParentBase?.cloneNode(true) as HTMLElement
 
-    const text =
-      manualParent?.getElementsByClassName("btn inline-flex popup-btn-ajax")[0];
+    const text = manualParent?.getElementsByClassName(
+      "btn inline-flex popup-btn-ajax"
+    )[0]
     if (!text) {
-      const dl = manualParent?.getElementsByClassName("btn inline-flex")[0];
-      path = dl?.attributes["href"].value;
+      const dl = manualParent?.getElementsByClassName("btn inline-flex")[0]
+      path = dl?.attributes["href"].value
       return
-    };
+    }
 
-    manualParentBase.parentNode.appendChild(manualParent);
-    manualParent.id = "better-nexus-active";
+    manualParentBase.parentNode.appendChild(manualParent)
+    manualParent.id = "better-nexus-active"
 
-    const search =
-      new URL(`https://localhost${text.attributes["href"].value}`).searchParams;
-    const file = search.get("id") || search.get("file_id");
-    const downloadUrl = new URL(window.location.href);
+    const search = new URL(`https://localhost${text.attributes["href"].value}`)
+      .searchParams
+    const file = search.get("id") || search.get("file_id")
+    const downloadUrl = new URL(window.location.href)
 
-    downloadUrl.searchParams.set("file_id", file);
-    downloadUrl.searchParams.set("tab", "files");
-    downloadUrl.searchParams.delete("fast");
+    downloadUrl.searchParams.set("file_id", file)
+    downloadUrl.searchParams.set("tab", "files")
+    downloadUrl.searchParams.delete("fast")
 
-    text.setAttribute("href", downloadUrl.href);
-    text.getElementsByClassName("flex-label")[0].innerHTML = "Quick Download";
-    text.classList.remove("popup-btn-ajax");
+    text.setAttribute("href", downloadUrl.href)
+    text.getElementsByClassName("flex-label")[0].innerHTML = "Quick Download"
+    text.classList.remove("popup-btn-ajax")
 
-    path = downloadUrl.href;
-  };
+    path = downloadUrl.href
+  }
 
   const addAutoDownload = () => {
     const interval = setInterval(() => {
-      const dl = document.getElementById("slowDownloadButton");
+      const dl = document.getElementById("slowDownloadButton")
 
       if (dl) {
-        clearInterval(interval);
-        setTimeout(() => dl.click(), 200);
+        clearInterval(interval)
+        setTimeout(() => dl.click(), 200)
       }
-    }, 200);
-  };
-  const getInstantDownloadUrl = async (file: string, gameId: string): Promise<string> => {
+    }, 200)
+  }
+  const getInstantDownloadUrl = async (
+    file: string,
+    gameId: string
+  ): Promise<string> => {
     let url = await fetch(
       "https://www.nexusmods.com/Core/Libs/Common/Managers/Downloads?GenerateDownloadUrl",
       {
-        "headers": {
-          "accept": "*/*",
+        headers: {
+          accept: "*/*",
           "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-          "x-requested-with": "XMLHttpRequest",
+          "x-requested-with": "XMLHttpRequest"
         },
-        "referrerPolicy": "strict-origin-when-cross-origin",
-        "body": `fid=${file}&game_id=${gameId}`,
-        "method": "POST",
-        "mode": "cors",
-        "credentials": "include",
-      },
-    ).then((r) => r.json()).then((r) => r.url);
-    return url;
-  };
+        referrerPolicy: "strict-origin-when-cross-origin",
+        body: `fid=${file}&game_id=${gameId}`,
+        method: "POST",
+        mode: "cors",
+        credentials: "include"
+      }
+    )
+      .then((r) => r.json())
+      .then((r) => r.url)
+    return url
+  }
   const addInstantDownload = async () => {
-    if (!path || document.getElementById("better-nexus-instant-download")) return;
-    let search = new URL(path).searchParams;
-    let file = search.get("id") ?? search.get("file_id");
-    let manualParentBase = document.getElementById("action-manual");
+    if (!path || document.getElementById("better-nexus-instant-download"))
+      return
+    let search = new URL(path).searchParams
+    let file = search.get("id") ?? search.get("file_id")
+    let manualParentBase = document.getElementById("action-manual")
 
-    let manualParent = manualParentBase.cloneNode(true) as HTMLElement;
+    let manualParent = manualParentBase.cloneNode(true) as HTMLElement
 
-    let text = manualParent
-      ?.getElementsByClassName("btn inline-flex")[0];
+    let text = manualParent?.getElementsByClassName("btn inline-flex")[0]
 
-    text.getElementsByClassName("flex-label")[0].innerHTML = "Instant Download";
+    text.getElementsByClassName("flex-label")[0].innerHTML = "Instant Download"
     text.id = "better-nexus-instant-download"
-    manualParentBase.parentNode.appendChild(manualParent);
+    manualParentBase.parentNode.appendChild(manualParent)
 
-    let url = await getInstantDownloadUrl(file, window.current_game_id);
+    let url = await getInstantDownloadUrl(file, window.current_game_id)
     if (!url) {
-      text.classList.add("disabled");
-      return;
-    };
-    manualParent.children[0].attributes["href"].value = url;
-
-    if (
-      window.location.search.includes("fast=true")
-    ) {
-      window.location.href = url;
+      text.classList.add("disabled")
+      return
     }
-  };
+    manualParent.children[0].attributes["href"].value = url
+
+    if (window.location.search.includes("fast=true")) {
+      window.location.href = url
+    }
+  }
   const waitForElement = (
-    selector: string,
+    selector: string
   ): Promise<HTMLCollectionOf<HTMLElement>> => {
     return new Promise((resolve) => {
       function waitFor() {
-        const element = document.getElementsByClassName(selector);
+        const element = document.getElementsByClassName(selector)
         if (element.length) {
-          resolve(element as HTMLCollectionOf<HTMLElement>);
+          resolve(element as HTMLCollectionOf<HTMLElement>)
         } else {
-          window.requestAnimationFrame(waitFor);
+          window.requestAnimationFrame(waitFor)
         }
       }
-      waitFor();
-    });
-  };
+      waitFor()
+    })
+  }
   const listDownloadButton = async () => {
-    let mods: HTMLCollectionOf<HTMLElement> = await waitForElement(
-      "mod-tile",
-    );
+    let mods: HTMLCollectionOf<HTMLElement> = await waitForElement("mod-tile")
     for (let mod of mods) {
-      let btn = document.createElement("button");
-      btn.innerText = "Download";
-      btn.classList.add("inline-flex", "btnsmall", "btn");
-      btn.style.marginLeft = "auto";
-      let elem = mod.getElementsByClassName("tile-data")[0];
-      if (elem.attributes["better-nexus-active"]) continue;
-      elem.attributes["better-nexus-active"] = "true";
-      let element = document.createElement("li");
-      element.classList.add("inline-flex");
-      element.appendChild(btn);
-      elem.children[0].appendChild(element);
-      let link = mod.getElementsByClassName("tile-name")[0].children[0].attributes["href"]
+      let btn = document.createElement("button")
+      btn.innerText = "Download"
+      btn.classList.add("inline-flex", "btnsmall", "btn")
+      btn.style.marginLeft = "auto"
+      let elem = mod.getElementsByClassName("tile-data")[0]
+      if (elem.attributes["better-nexus-active"]) continue
+      elem.attributes["better-nexus-active"] = "true"
+      let element = document.createElement("li")
+      element.classList.add("inline-flex")
+      element.appendChild(btn)
+      elem.children[0].appendChild(element)
+      let link =
+        mod.getElementsByClassName("tile-name")[0].children[0].attributes[
+          "href"
+        ]
       btn.onclick = async () => {
         try {
-          let text = await fetch(link.value).then(r => r.text());
-          const regex = /(https:\/\/www\.nexusmods\.com\/subnautica\/mods\/(\d+)\?tab=files&file_id=(\d+)|\/Core\/Libs\/Common\/Widgets\/ModRequirementsPopUp\?id=(\d+)&game_id=(\d+))/g;
-          const match = regex.exec(text);
+          let text = await fetch(link.value).then((r) => r.text())
+          const regex =
+            /(https:\/\/www\.nexusmods\.com\/subnautica\/mods\/(\d+)\?tab=files&file_id=(\d+)|\/Core\/Libs\/Common\/Widgets\/ModRequirementsPopUp\?id=(\d+)&game_id=(\d+))/g
+          const match = regex.exec(text)
 
           if (match) {
-            let url = match[0];
+            let url = match[0]
             if (!url.startsWith("https://")) {
-              url = "https://www.nexusmods.com" + url;
+              url = "https://www.nexusmods.com" + url
             }
-            let search = new URL(url).searchParams;
-            let modId = search.get("id") || search.get("file_id");
-            let dl = await getInstantDownloadUrl(modId, window.current_game_id);
-            let downloadAnchor = document.createElement("a");
-            downloadAnchor.href = dl;
-            downloadAnchor.click();
+            let search = new URL(url).searchParams
+            let modId = search.get("id") || search.get("file_id")
+            let dl = await getInstantDownloadUrl(modId, window.current_game_id)
+            let downloadAnchor = document.createElement("a")
+            downloadAnchor.href = dl
+            downloadAnchor.click()
           } else {
-            alert("I could not find the download link.");
+            alert("I could not find the download link.")
           }
         } catch (e) {
-          alert(e);
+          alert(e)
         }
-      };
+      }
     }
-  };
+  }
   if (config.listDownloadButton) {
-    listDownloadButton();
+    listDownloadButton()
   }
   if (config.quickDownloadButton) {
-    addQuickDownload();
+    addQuickDownload()
   }
   if (config.autoDownload) {
-    addAutoDownload();
+    addAutoDownload()
   }
   if (config.superQuickDownload) {
-    addInstantDownload();
+    addInstantDownload()
   }
 
   if (path && !config.superQuickDownload) {
-    if (
-      window.location.search.includes("fast=true")
-    ) {
-      window.location.href = path;
+    if (window.location.search.includes("fast=true")) {
+      window.location.href = path
     }
   }
   function removeAllPremiumBanners() {
-    let elemns = document.querySelectorAll("section .premium-block");
+    let elemns = document.querySelectorAll("section .premium-block")
     for (let elem of elemns) {
-      elem.classList.add("hidden");
-      elem.remove();
+      elem.classList.add("hidden")
+      elem.remove()
     }
   }
   if (config.removePremiumBanners) {
     setTimeout(() => {
-      removeAllPremiumBanners();
-    }, 200);
-    removeAllPremiumBanners();
+      removeAllPremiumBanners()
+    }, 200)
+    removeAllPremiumBanners()
   }
-};
+}
 
-export default windowChanger;
+export default windowChanger
