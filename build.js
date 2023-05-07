fb2cb11de451a9450346e45af7048f9e5b2e65de
Tricked <tricked@tricked.pro>
2023-05-07 13:25:31 +0200

    building

--- build.js
+++ build.js
@@ -1,24 +1,30 @@
-import { execSync } from "child_process";
-import { mkdir, readdir, readFile, rename } from "fs/promises";
+import { execSync } from "child_process"
+import { renameSync } from "fs"
+import { mkdir, readFile, readdir, rename } from "fs/promises"
 
-console.log("Reading package.json...");
-const { name, version } = JSON.parse(await readFile("./package.json", "utf-8"));
+console.log("Reading package.json...")
+const { name, version } = JSON.parse(await readFile("./package.json", "utf-8"))
 
-console.log("Building...");
+console.log("Building...")
 // run all build script for browsers
 execSync("pnpm build", {
-  stdio: "inherit",
-});
+  stdio: "inherit"
+})
 
-console.log("Done building");
+console.log("Done building")
 // create dist folder if it doesn't exist already
-await mkdir("./dist").catch(() => {});
+await mkdir("./dist").catch(() => {})
+
+await mkdir("./build/x/").catch(() => {})
+
+renameSync("build/chrome-mv3-prod", `build/x/${name}-${version}`)
+
 // rename all zip files to include version number and browser name
 for await (let file of await readdir("./build")) {
-  if (!file.endsWith(".zip")) continue;
-  let browser = file.split("-")[0];
+  if (!file.endsWith(".zip")) continue
+  let browser = file.split("-")[0]
   await rename(
     `./build/${file}`,
-    `./dist/${name}-${version}-${browser}.zip`,
-  ).catch(console.error);
+    `./dist/${name}-${version}-${browser}.zip`
+  ).catch(console.error)
 }
